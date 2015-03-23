81cc7052d1d045937abb48e4ef67390fd7b1bf0a
mertakanay <mert.akanay@live.com>
2015-03-23 12:49:57 -0700

    final stage

--- Superheropedia/ViewController.m
+++ Superheropedia/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property NSArray *superheroArray;
 
 @end
 
@@ -16,12 +19,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //Array of NSDictionaries
+//    self.superheroArray = [NSArray arrayWithObjects:[NSDictionary dictionaryWithObjectsAndKeys:@"Batman",@"name",@"28",@"age", nil],[NSDictionary dictionaryWithObjectsAndKeys:@"Ironman",@"name",@"34",@"age", nil], nil];
+    //first string we write is the object, second is the key. For third and forth we repeat the same order.
+
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mobile-makers-lib/superheroes.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+
+        self.superheroArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+
+        [self.tableView reloadData];
+
+    }]; //we are making a call to the internet so we are using AsynchronousRequest instead of SynchronousRequest.
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
+    NSDictionary *dictionary = [self.superheroArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = [dictionary objectForKey:@"name"]; //calls the object connected to this key("name")
+    cell.detailTextLabel.text = [dictionary objectForKey:@"description"]; //we changed the key because its given as "description" on JSON website.
+    cell.detailTextLabel.numberOfLines = 0;
+    NSURL *url = [NSURL URLWithString:[dictionary objectForKey:@"avatar_url"]];
+    cell.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+
+    return cell;
 }
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.superheroArray.count;
+}
+
 
 @end
