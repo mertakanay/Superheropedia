45ed57f9d737baf840d769523b8a390c173e278b
mertakanay <mert.akanay@live.com>
2015-03-23 10:33:10 -0700

    Initial Commit

---
+++ Superheropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Superheropedia
+//
+//  Created by Mert Akanay on 23.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Superheropedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Superheropedia
+//
+//  Created by Mert Akanay on 23.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
